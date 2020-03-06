70add075ce4bf4fe4f62a12bfa4a708718a73b33
kevinnejad <keyv.krmn@gmail.com>
2020-03-06 01:24:07 +0000

    add more SDEs

--- MATLAB Code/met_hist.m
+++ MATLAB Code/met_hist.m
@@ -18,7 +18,7 @@ a = 0.5;
 b = 2;
 Xzero = 1;
 
-M = 5e4;    % number of paths
+M = 10e5;    % number of paths
 
 texit = zeros(M,1);
 
@@ -35,14 +35,65 @@ for s = 1:M
       %t
 end
 
+
+%%%%%%% Euler Maryama %%%%%%
+texit_EM = zeros(M,1);
+
+for s = 1:M
+     s
+     X = Xzero;
+     t = 0;
+     while X > a & X < b,
+       dW = sqrt(dt)*randn;                         % increments
+       X = X + dt*mu*X+sigma*X*dW;
+       t = t + dt;
+     end
+     texit_EM(s) = t - 0.5*dt;
+      %t
+end
+
+%%%%%%% Milstein %%%%%%
+texit_Mils = zeros(M,1);
+
+for s = 1:M
+     s
+     X = Xzero;
+     t = 0;
+     while X > a & X < b,
+       dW = sqrt(dt)*randn;                         % increments
+       X = X + dt*mu*X+sigma*X*dW + 0.5*sigma*X*sigma*(dW^2 - dt);
+       t = t + dt;
+     end
+     texit_Mils(s) = t - 0.5*dt;
+      %t
+end
+
+
+disp('Exact Solution Summary')
 tmean = mean(texit)
 tstd = std(texit)
 
 cileft = tmean - 1.96*tstd/sqrt(M)
 ciright = tmean + 1.96*tstd/sqrt(M)
 
-%%% Compute exact value %%% 
+disp('Euler Maryama Summary')
+tmean_EM = mean(texit_EM)
+tstd_EM = std(texit_EM)
 
+cileft_EM = tmean - 1.96*tstd_EM/sqrt(M)
+ciright_EM = tmean + 1.96*tstd_EM/sqrt(M)
+
+disp('Milstein Summary')
+tmean_Mils = mean(texit_Mils)
+tstd_Mils = std(texit_Mils)
+
+cileft_Mils = tmean - 1.96*tstd_Mils/sqrt(M)
+ciright_Mils = tmean + 1.96*tstd_Mils/sqrt(M)
+
+
+
+
+%%% Compute exact value %%% 
 %%% Temporary varables to break up the formula %%%
 temp1 = 1/(0.5*sigma^2 - mu);
 temp2 = log(Xzero/a);
@@ -55,8 +106,3 @@ temp5 = log(b/a);
 
 %%% Mean Hitting time formula %%%
 texact = temp1*( temp2 - (temp3./temp4)*temp5)
-
-hist(texit,50)
-xlabel('First exit time','FontSize',16,'FontWeight','Bold') 
-set(gca,'FontWeight','Bold','FontSize',12)
-
